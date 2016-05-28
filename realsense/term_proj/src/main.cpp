a254d46f21a399d50cd71262806d449d6a4144c9
Thomas Tsai <thomas@life100.cc>
2016-05-28 16:44:28 +0800

    adding intel RSSDK

---
+++ realsense/term_proj/src/main.cpp
@@ -0,0 +1,121 @@
+#include <stdio.h>
+#include <iostream>
+
+#include "RssdkHandler.h"
+
+ModelViewController::HandsModel* handModel;
+ModelViewController::IView* openGLView;
+ModelViewController::HandsController* controller;
+RssdkHandler* rssdkHandler;
+
+
+void releaseAll();
+
+/** @brief The Hand Module consists two separate tracking modes (Full-hand & Extremities) , 
+each designed to solve a different use-case, while the Cursor Module has a single tracking mode
+Tracking mode:
+cursor : 2 hands, fast speed, SR300 ??110 cm
+	Cursor point in 2D and 3D, alerts, gestures
+Full Hand : 2 hands, slower hand speed, SR300 ??85 cm
+	Segmentation image, extremity points, hand side, alerts, 
+	joints info, fingers info, openness, gestures
+Extremities : 2 hands, medium hand speed, SR300 ??120 cm
+	Segmentation image, extremity points, hand side, alerts
+
+command line  : -full , full hand mode
+*/
+int rs_main(int argc, char** argv)
+{
+	bool isFullHand = false;
+	if(argc == 2)
+	{
+		if (strcmp(argv[1],"-full")==0)
+		{
+			isFullHand = true;
+		}
+	}
+
+	pxcStatus status = PXC_STATUS_ALLOC_FAILED;
+
+	// Create hand model
+	handModel = new ModelViewController::HandsModel();
+	if(!handModel)
+	{
+		std::printf("Failed at Initialization\n");
+		releaseAll();
+		return -1;
+	}
+
+	// Create Openglview which implements IView (allows creations of different views)
+	openGLView = new ModelViewController::OpenGLView(isFullHand);
+	if(!openGLView)
+	{
+		std::printf("Failed at Initialization\n");
+		releaseAll();
+		return -1;
+	}
+
+	// When using sequence, change useSequence to true and apply sequencePath with sequence path
+	bool useSequence = false;
+	pxcCHAR* sequencePath = L"Insert Sequence Path Here";
+	
+
+	// Bind controller with model and view and start playing
+	controller = new ModelViewController::HandsController(handModel,openGLView);
+	if(!controller)
+	{
+		std::printf("Failed at Initialization\n");
+		releaseAll();
+		return -1;
+	}
+
+	rssdkHandler = new RssdkHandler(controller,handModel,openGLView);
+
+	if(useSequence)
+	{
+		if(rssdkHandler->Init(isFullHand,sequencePath) == PXC_STATUS_NO_ERROR)
+		{
+			rssdkHandler->Start();
+		}
+
+		else
+		{
+			std::printf("Failed at Initialization\n");
+			releaseAll();
+			return -1;
+		}
+	}
+
+	else
+	{
+		if(rssdkHandler->Init(isFullHand) == PXC_STATUS_NO_ERROR)
+		{
+			rssdkHandler->Start();
+		}
+
+		else
+		{
+			std::printf("Failed at Initialization\n");
+			releaseAll();
+			return -1;
+		}
+	}
+
+	releaseAll();
+    return 0;
+}
+
+void releaseAll()
+{
+	//delete all pointers
+	if(handModel)
+		delete handModel;
+	if(openGLView)
+		delete openGLView;
+	if(controller)
+		delete controller;
+	if(rssdkHandler)
+		delete rssdkHandler;	
+}
+
+
---
+++ realsense/term_proj/src/model_rendered.cpp
@@ -0,0 +1,250 @@
+// Std. Includes
+#include <string>
+
+// GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+// GLFW
+#include <GLFW/glfw3.h>
+
+// GL includes
+#include <learnopengl/shader.h>
+#include <learnopengl/camera.h>
+#include <learnopengl/model.h>
+
+// GLM Mathemtics
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+// Other Libs
+#include <SOIL.h>
+#include <learnopengl/filesystem.h>
+
+// Properties
+GLuint screenWidth = 800, screenHeight = 600;
+
+// Function prototypes
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void Do_Movement();
+
+// Camera
+//Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+Camera camera(glm::vec3(1.0f, 1.0f, 2.0f));
+bool keys[1024];
+GLfloat lastX = 400, lastY = 300;
+bool firstMouse = true;
+
+GLfloat deltaTime = 0.0f;
+GLfloat lastFrame = 0.0f;
+
+//#define VIEWPORT_WIDTH              1280
+//#define VIEWPORT_HEIGHT             800
+
+void glut_init(void)
+{
+	char *myargv[1];
+	int myargc = 1;
+	myargv[0] = "intel realsense";
+	glutInit(&myargc, myargv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(screenWidth, screenHeight);
+}
+
+void renderBitmapString(float x, float y, void *font, const char *string) {
+	const char *c;
+	glRasterPos2f(x, y);
+	for (c = string; *c != '\0'; c++) {
+		glutBitmapCharacter(font, *c);
+	}
+}
+
+void drawFps(int m_fps=30)
+{
+#if 0
+	std::string line = "FPS = ";
+	line.append(std::to_string(m_fps));
+	//glPushAttrib(GL_CURRENT_BIT);
+	//glDisable(GL_LIGHTING);
+	glColor3f(1.0, 0.f, 0.f);
+	renderBitmapString(200, 200, GLUT_BITMAP_8_BY_13, line.c_str());
+	//glEnable(GL_LIGHTING);
+	glDisable(GL_LIGHTING);
+	std::string line = "--- PAUSE ---";
+	glColor3f(1.f, 0.f, 0.f);
+	renderBitmapString(0.1f, 0.5f, GLUT_BITMAP_TIMES_ROMAN_24, line.c_str());
+	glEnable(GL_LIGHTING);
+#endif
+
+	glColor3f(1.f, 1.f, 1.f);
+	renderBitmapString(0.9f, 0.49f, GLUT_BITMAP_8_BY_13, "Instructions:");
+	renderBitmapString(0.9f, 0.46f, GLUT_BITMAP_8_BY_13, "-------------");
+	renderBitmapString(0.9f, 0.43f, GLUT_BITMAP_8_BY_13, "Full Hands Mode: Via Command Line - Run \"FF_Hands3DViewer.exe -full\"");
+	renderBitmapString(0.9f, 0.40f, GLUT_BITMAP_8_BY_13, "Left Mouse Button - Hold to rotate view");
+	renderBitmapString(0.9f, 0.37f, GLUT_BITMAP_8_BY_13, "Right Mouse Button - Click to reset view");
+	renderBitmapString(0.9f, 0.34f, GLUT_BITMAP_8_BY_13, "Mouse Wheel Up/Down - Zoom in/out");
+	renderBitmapString(0.9f, 0.31f, GLUT_BITMAP_8_BY_13, "F1 - Pause/Play");
+	renderBitmapString(0.9f, 0.28f, GLUT_BITMAP_8_BY_13, "F3 - <Cursor Only> Shorten trail");
+	renderBitmapString(0.9f, 0.25f, GLUT_BITMAP_8_BY_13, "F4 - <Cursor Only> Extend trail");
+
+	//glRasterPos2i(100, 120);
+	//glColor4f(0.0f, 0.0f, 1.0f, 1.0f);
+	//glutBitmapString(GLUT_BITMAP_HELVETICA_18, "text to render");
+}
+
+// Options
+GLboolean blinn = false;
+
+int rs_main(int argc, char** argv);
+// The MAIN function, from here we start our application and run our Game loop
+int main(int argc, char **argv)
+{
+//	rs_main(argc, argv);///////////
+	// Init GLFW
+    glfwInit();
+	//glfw has no "drawing text function", so init glut for drawing text
+	glut_init();
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    GLFWwindow* window = glfwCreateWindow(screenWidth, screenHeight, "DIP2016 HARP", nullptr, nullptr); // Windowed
+    glfwMakeContextCurrent(window);
+
+    // Set the required callback functions
+    glfwSetKeyCallback(window, key_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+
+    // Options
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+    // Initialize GLEW to setup the OpenGL Function pointers
+    glewExperimental = GL_TRUE;
+    glewInit();
+
+    // Define the viewport dimensions
+	//glfwGetFramebufferSize(window, &screenWidth, &screenHeight);
+    glViewport(0, 0, screenWidth, screenHeight);
+
+    // Setup some OpenGL options
+    glEnable(GL_DEPTH_TEST);
+
+    // Setup and compile our shaders
+    Shader shader("shader.vs", "shader.frag");
+
+	// Light source
+	glm::vec3 lightPos(0.0f, 0.0f, 0.0f);
+    // Load models
+    //Model ourModel(FileSystem::getPath("resources/objects/nanosuit/nanosuit.obj").c_str());
+	Model ourModel(FileSystem::getPath("resources/objects/cyborg/cyborg.obj").c_str());
+	//Model ourModel(FileSystem::getPath("resources/objects/jeep/jeep1.ms3d").c_str());
+	//Model ourModel(FileSystem::getPath("resources/objects/harp/harp.3ds").c_str());
+	//Model ourModel(FileSystem::getPath("resources/objects/monkey/monkey.obj").c_str());
+	//Model ourModel(FileSystem::getPath("resources/objects/planet/planet.obj").c_str());
+    // Draw in wireframe
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	
+    // Game loop
+    while(!glfwWindowShouldClose(window))
+    {
+        // Set frame time
+        GLfloat currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        // Check and call events
+        glfwPollEvents();
+        Do_Movement();
+
+        // Clear the colorbuffer
+        glClearColor(0.2f, 0.2f, 0.35f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        shader.Use();   // <-- Don't forget this one!
+        // Transformation matrices
+        glm::mat4 projection = glm::perspective(camera.Zoom, (float)screenWidth/(float)screenHeight, 0.1f, 100.0f);
+        glm::mat4 view = camera.GetViewMatrix();
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "view"), 1, GL_FALSE, glm::value_ptr(view));
+
+        // Draw the loaded model
+        glm::mat4 model;
+        //model = glm::translate(model, glm::vec3(0.0f, -1.75f, 0.0f)); // Translate it down a bit so it's at the center of the scene
+        //model = glm::scale(model, glm::vec3(0.2f, 0.2f, 0.2f));	// It's a bit too big for our scene, so scale it down
+		model = glm::translate(model, glm::vec3(0.0f, -1.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(0.1f, 0.1f, 0.1f));
+		glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        ourModel.Draw(shader);       
+
+		// Set light uniforms
+		glUniform3fv(glGetUniformLocation(shader.Program, "lightPos"), 1, &lightPos[0]);
+		glUniform3fv(glGetUniformLocation(shader.Program, "viewPos"), 1, &camera.Position[0]);
+		glUniform1i(glGetUniformLocation(shader.Program, "blinn"), blinn);
+		//drawFps();
+        // Swap the buffers
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
+#pragma region "User input"
+
+// Moves/alters the camera positions based on user input
+void Do_Movement()
+{
+    // Camera controls
+    if(keys[GLFW_KEY_W])
+        camera.ProcessKeyboard(FORWARD, deltaTime);
+    if(keys[GLFW_KEY_S])
+        camera.ProcessKeyboard(BACKWARD, deltaTime);
+    if(keys[GLFW_KEY_A])
+        camera.ProcessKeyboard(LEFT, deltaTime);
+    if(keys[GLFW_KEY_D])
+        camera.ProcessKeyboard(RIGHT, deltaTime);
+}
+
+// Is called whenever a key is pressed/released via GLFW
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	if (key < 0) return;//boundary check
+	if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+
+    if(action == GLFW_PRESS)
+        keys[key] = true;
+    else if(action == GLFW_RELEASE)
+        keys[key] = false;	
+}
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+    if(firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    GLfloat xoffset = xpos - lastX;
+    GLfloat yoffset = lastY - ypos; 
+    
+    lastX = xpos;
+    lastY = ypos;
+
+    camera.ProcessMouseMovement(xoffset, yoffset);
+}	
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    camera.ProcessMouseScroll(yoffset);
+}
+
+#pragma endregion
